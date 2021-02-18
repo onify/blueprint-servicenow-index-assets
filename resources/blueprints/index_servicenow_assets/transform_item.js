dec010d33953b27bd3dc3a92b1d585f53d467498
Robert Lundsten <robert@onify.co>
2021-02-18 21:54:14 +0100

    Create transform_item.js

---
+++ resources/blueprints/index_servicenow_assets/transform_item.js
@@ -0,0 +1,24 @@
+const itemAssets = [];
+environment.output._getServiceNowAssets.records.forEach(async function(snAsset) {
+    console.log(snAsset.display_name.value);
+    const itemAsset = {
+        type: "asset",
+        key: snAsset.sys_id.value,
+        name: snAsset.display_name.value,
+        tag: ["servicenow", "asset", snAsset.sys_class_name.display_value.toLowerCase()],
+        attribute: {
+            cost_center: snAsset.cost_center.display_value,
+            company: snAsset.company.display_value,
+            department: snAsset.department.display_value,
+            location: snAsset.location.display_value,
+            assigned_to: snAsset.assigned_to.display_value,
+            model: snAsset.model.display_value,
+        },
+        cost: {
+            price: snAsset.cost.value
+        }
+    };
+    itemAssets.push(itemAsset);
+})
+
+next(null, itemAssets);
